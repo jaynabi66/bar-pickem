f1aac8b0a4e7506237211aefaf3afa440d10372d
Jay Nabi <jaynabi66@gmail.com>
2025-10-24 21:43:59 -0700

    Display image and file name when selecting pickem

--- scan/script.js
+++ scan/script.js
@@ -1,3 +1,32 @@
+const pickemImages = document.getElementById("pickemImages");
+const pickemLabel = document.getElementById("pickemLabel");
+const pickemInput = document.getElementById("pickemInput");
+
+pickemInput.addEventListener("change", function(event) {
+    const files = [...event.target.files];
+    if (files.length > 0) {
+        const reader = new FileReader();
+        reader.onload = function(e) {
+            const image = document.createElement("img");
+            image.onclick = function() {
+                pickemInput.click();
+            };
+            image.className = "scan__image";
+            image.src = e.target.result;
+            
+            pickemImages.innerHTML = "";
+            pickemImages.appendChild(image);
+        };
+
+        const file = files[0];
+        reader.readAsDataURL(file);
+        pickemLabel.textContent = file.name;
+    } else {
+        pickemImages.innerHTML = "";
+        pickemLabel.textContent = "Choose file";
+    }
+});
+
 function navigateResults() {
     window.location.assign("../results/index.html");
 }
