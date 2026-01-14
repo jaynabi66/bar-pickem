35ed609c67436f598c5d7a5072c650c4f6f2065b
Jay Nabi <jaynabi66@gmail.com>
2026-01-13 20:16:24 -0800

    Update the API url.

--- src/pages/Scan/scanHelpers.js
+++ src/pages/Scan/scanHelpers.js
@@ -70,7 +70,7 @@ export async function processImage () {
     const arrayBuffer = await imageResponse.arrayBuffer();
     const byteArray = new Uint8Array(arrayBuffer);
 
-    const processResponse = await fetch("/api/process", {
+    const processResponse = await fetch("https://gambit-bjf8gahhfmb6crdr.canadacentral-01.azurewebsites.net/api/process", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: byteArray,
